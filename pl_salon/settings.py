f0e7251c76cfe02ee775580d8ada7b0db9b86971
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-01-16 12:10:58 -0600

    update:database

--- pl_salon/settings.py
+++ pl_salon/settings.py
@@ -91,15 +91,13 @@ WSGI_APPLICATION = 'pl_salon.wsgi.app'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+
 DATABASES = {
-    'default':  {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db_pl_salon',
-        'USER':'postgres',
-        'PASSWORD':'Ronald2121/24-',
-        'HOST':'localhost',
-        'PORT':'5432',
-    }
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        conn_max_age=600
+    )
 }
 
 
