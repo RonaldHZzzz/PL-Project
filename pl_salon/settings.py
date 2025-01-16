cc22ddbdb0422d04ebad618d2dc74e96fa950a19
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-01-15 23:23:56 -0600

    update

--- pl_salon/settings.py
+++ pl_salon/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         'NAME': 'db_pl_salon',
         'USER':'postgres',
         'PASSWORD':'Ronald2121/24-',
-        'HOST':'localhost',
+        'HOST':'http://127.0.0.1:8000/upda',
         'PORT':'5432',
     }
 }
