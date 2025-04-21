a164ece2440a876ef2aaca0bc1eff72a5e4d12bf
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-04-20 23:02:23 -0600

    feat: environment variable

--- pl_salon/settings.py
+++ pl_salon/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -95,24 +97,17 @@ WSGI_APPLICATION = 'pl_salon.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://USER:PASSWORD@HOST:5032/db_pl_salon',
-        conn_max_age=600
-    )
-    
-    
-    #     'default':  {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'db_pl_salon',
-    #     'USER':'postgres',
-    #     'PASSWORD':'Ronald2121/24-',
-    #     'HOST':'localhost',
-    #     'PORT':'5432',
-    # }
-
+      'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
 
 }
-
+print(f'Current database engine is {DATABASES["default"]["NAME"]}')
 
 
 
