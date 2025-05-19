ea38a1b20275fa2a15e8dd07fdbefb1fd7eecbfd
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-05-18 21:56:24 -0600

    fix error date 1

--- users/models.py
+++ users/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+
 # Create your models here.
 
 class Trabajo(models.Model):
-    usuario= models.ForeignKey(User, on_delete=models.CASCADE) #relacion con el usario
-    trabajo=models.CharField(max_length=255)#campo de trabajo
-    monto=models.DecimalField(max_digits=10, decimal_places=2)
-    fecha_registro= models.DateField(auto_now_add=True)
-    
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con el usuario
+    trabajo = models.CharField(max_length=255)  # Descripción del trabajo
+    monto = models.DecimalField(max_digits=10, decimal_places=2)  # Monto del trabajo
+    # Guardar la fecha local, evitando desfaces de UTC
+    fecha_registro = models.DateField(
+        default=timezone.localdate,
+        editable=False
+    )
+
     def __str__(self):
-        return f"{self.trabajo} - {self.usuario.username}"
-    
-    
+        return f"{self.trabajo} - {self.usuario.username}"  
+
+
 class Descuentos(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.CASCADE)  # Relación con el usuario
     descripcion = models.CharField(max_length=255)  # Descripción del descuento
     descuento = models.DecimalField(max_digits=10, decimal_places=2)  # Monto del descuento
-    fecha_registro_descuento = models.DateField(auto_now_add=True)  # Fecha de registro del descuento
+    # Guardar la fecha local, evitando desfaces de UTC
+    fecha_registro_descuento = models.DateField(
+        default=timezone.localdate,
+        editable=False
+    )
 
     def __str__(self):
         return f"Descuento de {self.descripcion} para {self.usuario.username}"
