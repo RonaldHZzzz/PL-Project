06ecb5b749fa30b9de2959317160174276df9d62
RonaldHZzzz <ronaldhernandez212121@gmail.com>
2025-06-05 21:21:43 -0600

    debug

--- pl_salon/settings.py
+++ pl_salon/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY',default='your secret key')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = False
 
 ALLOWED_HOSTS = ['web-production-995c.up.railway.app','localhost','127.0.0.1','plsalon-gfbegubne8g0fzdz.canadacentral-01.azurewebsites.net']
 
